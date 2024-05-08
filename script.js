7b2f4d839a815d5dbd39764c88b39a6cd0e9f4f1
Gowtham <154532599+GowthamSaiP@users.noreply.github.com>
2024-05-08 19:47:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function pbclick(){
+    document.getElementById("pb").style.backgroundImage = "url('bg/pbbg.jpg')";
+}
+function dpclick() {
+    document.getElementById("dp").style.backgroundImage = "url('bg/dpbg.jpg')";
+}
+function ngclick() {
+    document.getElementById("ng").style.backgroundImage = "url('bg/ngbg.jpg')";
+}
